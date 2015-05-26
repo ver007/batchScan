c6362e922944d5793b341a75e04c9be8f9322f16
he1m4n6a <he1m4n6a@163.com>
2015-05-26 23:47:05 +0800

    update test

--- node/test.py
+++ node/test.py
@@ -2,8 +2,8 @@
 #coding=utf8
 
 dict_rule = {
-    'scan_rule' = "",
-    'res_rule' = "",
-    'payload' = "",
-    'method' = ""
+    'scan_rule' : '',
+    'res_rule'  : '',
+    'payload' : '',
+    'method' : ''
 }

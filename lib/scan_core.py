638ebee1433d83be91d65ac26ac607d127c841f9
he1m4n6a <he1m4n6a@163.com>
2015-05-26 23:31:35 +0800

    update py

--- lib/scan_core.py
+++ lib/scan_core.py
@@ -2,18 +2,17 @@
 #coding=utf8
 
 class AbstractScan:
-    def __init__(self, options, addr_file, out_file):
+    def __init__(self, options):
         self.options = options
-        self.addr_file = addr_file
-        self.out_file = out_file
 
     '''扫描url具体方法'''
-    def scan_url(self, payload, addr_file, out_file):
+    def scan_url(self, payload):
         scan_rule = self.options.scan_rule
         res_rule = self.options.res_rule
         method = self.options.method
+        threadnum = self.options.thread
         url_list = []
-        with open(addr_file) as urltxt:
+        with open('url.txt') as urltxt:
             url_list = urltxt.read().split('\n')
-        scanner = ScanTool(url_list, payload, scan_rule, res_rule, method, out_file)
+        scanner = ScanTool(url_list, payload, scan_rule, res_rule, method, threadnum)
         scanner.scan()
